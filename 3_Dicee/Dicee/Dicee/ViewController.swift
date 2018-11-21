4bc42c5f877ee87ecbaf2c395249273dde28943a
prcdevs <rahulsharmacs50@gmail.com>
2018-11-21 16:28:32 +0530

    one function for changing dice faces

--- 3_Dicee/Dicee/Dicee/ViewController.swift
+++ 3_Dicee/Dicee/Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
@@ -17,16 +19,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2   : UIImageView!
 
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        updateDiceImages()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        //this will get executed when the roll button gets pressed
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages()
+    {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
 
 
