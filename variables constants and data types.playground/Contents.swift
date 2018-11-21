31944c7de5a2abaa354f31f21f13c9488859d684
prcdevs <rahulsharmacs50@gmail.com>
2018-11-21 16:01:41 +0530

    data types and outlets

---
+++ variables constants and data types.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+var myAge : Int = 23
+myAge = 24
+
+let myName : String = "Rahul"
+let myAgeInTenYears = myAge + 10
+let myFullName = "\(myName) Sharma"
+
+let myDetails = "\(myFullName), \(myAge)"
+
+let booleans : Bool = true
+let floadtingPoint : Float = 10.0
+let doubleNumber : Double = 10.000000009
+
