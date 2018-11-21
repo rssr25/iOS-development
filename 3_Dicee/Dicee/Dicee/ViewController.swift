4ae06d251623bee5b535b9c79ae060bed87f26ab
prcdevs <rahulsharmacs50@gmail.com>
2018-11-21 14:37:15 +0530

    Dicee app initial push

---
+++ 3_Dicee/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Pentagram Research on 21/11/18.
+//  Copyright © 2018 Rahul Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
