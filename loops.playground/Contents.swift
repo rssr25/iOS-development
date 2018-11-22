1bcbd24a365dfaef03bff935be6deeb402195992
prcdevs <rahulsharmacs50@gmail.com>
2018-11-22 12:41:15 +0530

    fibonacci

--- loops.playground/Contents.swift
+++ loops.playground/Contents.swift
@@ -30,7 +30,7 @@ for number in 1 ..< 10 where number % 2 == 0
 }
 
 
-lyrics of a song
+//lyrics of a song
 
 for number in (1 ..< 100).reversed()
 {
@@ -40,17 +40,41 @@ for number in (1 ..< 100).reversed()
 func beerSong(forThisManyBottlesOfBeer totalNumberOfBottles : Int) -> String
 {
     var lyrics : String = ""
-    
+
     for number in (1 ... totalNumberOfBottles).reversed()
     {
         let newLine : String = "\(number) bottles of beer on the wall, \(number) bottles of beer. \n Take one down and pass it around, \(number - 1) bottles of beer on the wall.\n\n"
-        
+
         lyrics += newLine
     }
-    
+
     lyrics += "No more bottles of beer on the wall, no more bottles of beer. \n Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    
+
     return lyrics
 }
 
 print(beerSong(forThisManyBottlesOfBeer : 1000))
+
+
+//FIBONACCI
+
+func fibonacci(until n : Int)
+{
+    var first : Int = 0;
+    var second : Int = 1;
+    
+    print(0)
+    print(1)
+    
+    for _ in 0 ... n
+    {
+        let fibonacci = first + second;
+        first = second;
+        second = fibonacci;
+        
+        print(fibonacci)
+    }
+    
+}
+
+fibonacci(until: 20)
